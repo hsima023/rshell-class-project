71d4a08ae6b2f1eb50a9c0a24a2c66cdccf5a2fd
hsima023 <hsima003@ucr.edu>
2020-02-13 04:05:43 -0800

    Fixed #2: main function is created

--- src/main.cpp
+++ src/main.cpp
@@ -6,25 +6,29 @@
 
 #include <boost/tokenizer.hpp>
 #include "tokenizer.h"
+#include "executor.hpp"
 
 using namespace std;
 using namespace boost;
 
 int main() {
 	string userInput;
-	
-	cout << "$ ";
-	getline(cin, userInput);	
-	
-	char* argv[userInput.size()];
+	while(1){
+		cout << "$ ";
+		getline(cin, userInput);	
+		char *argv[1024];
+		Tokenizer(userInput, argv);
+		if (strcmp(argv[0], "exit") == 0){
+			exit(EXIT_SUCCESS);
+		}
+		int i = 0;
+        	while (argv[i] != NULL) {
+                	cout << argv[i] << endl;
+                	i++;
+        	}
+		Executor ex(argv);
+		ex.execute(argv);
 
-	Tokenizer(userInput, argv);
-
-	int i = 0;
-        while (argv[i] != NULL) {
-                cout << argv[i] << endl;
-                i++;
-        }
-
+	}	
 	return 0;
 }
