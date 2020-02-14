90b71d9ac2a905c362612cccfd119e708d58a2ee
hsima023 <hsima003@ucr.edu>
2020-02-14 14:13:09 -0800

    Updated orToken class

--- src/orToken.hpp
+++ src/orToken.hpp
@@ -2,39 +2,43 @@
 #define OR_TOKEN_HPP
 
 #include "token.hpp"
+#include <string>
 
 class orToken: public Token {
 	public:
-	bool isExist(char** argv)
+	bool isExist(char** argv, int num)
 	{
         	bool equal =false;
-        	for (int i = 0;argv[i] != "\n"; i++){
-                	if(argv[i] == "||"){
+		int i = num;
+		const char *sign[] = { "||" };
+        //	for (int i = 0;argv[i] != "\n"; i++){
+                	if(*argv[i] == *sign[0]){
                         	equal = true;
-                        	break;
+                        //	break;
                 	}
-        	}
+        	
         	return equal;
 	}
-	void logic()
+	void logic(char **argv, char **temp)
 	{
-        	char *temp[8];
         	const char *Or[] = { "||"};
+		string connector = "NULL";
         	for(int i = 0; argv[i] != NULL; i++){
                 	if(*argv[i] == *Or[0]){
                         	argv[i] = NULL;
                         	i++;
+				int k = 0;
                         	while(argv[i] != NULL){
-                                	int j = 0;
-                                	temp[j] = argv[i];
+                                	temp[k] = argv[i];
                                 	argv[i] = NULL;
                                 	i++;
-                                	j++;
+                                	k++;
                         	}
-                	break;
+				temp[k] = NULL;
+			
+                		break;
                 	}
         	}
-//      if(execute(argv)); then execute(temp);
  	}
 };
 
