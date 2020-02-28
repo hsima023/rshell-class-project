a0f4d912503fb167049fc7e3a0ef86e736d30395
hsima023 <hsima003@ucr.edu>
2020-02-27 16:37:38 -0800

    Added more unit tests

--- unit_tests/token_test.hpp
+++ unit_tests/token_test.hpp
@@ -11,6 +11,10 @@
 #include "../src/orToken.hpp"
 #include "../src/cmdToken.cpp"
 #include "../src/smcolonToken.hpp"
+#include "../src/bracketToken.hpp"
+#include "../src/testToken.hpp"
+#include "../src/parenToken.hpp"
+#include <string.h>
 
 TEST(cmdExistvalue, TokEvaluate) {
 	char *input[2] = { "ls"};
@@ -48,10 +52,143 @@ TEST(orExistvalue, TokEvaluate) {
 }
 
 TEST(smcolonExistvalue, TokEvaluate) {
-        char *input[2] = { "ls"};
+        char *input[2] = { ";"};
         int index = 0;
         smcolonToken smcolon;
         EXPECT_EQ(smcolon.isExist(input, index), true);
+}
+
+TEST(parenExistvalue, TokEvaluate) {
+        char *input[2] = { "("};
+        int index = 0;
+        parenToken paren;
+        EXPECT_EQ(paren.isExist(input, index), true);
+}
+
+TEST(bracketExistvalue, TokEvaluate) {
+        char *input[2] = {"["};
+        int index = 0;
+        bracketToken brack;
+        EXPECT_EQ(brack.isExist(input, index), true);
+}
+
+TEST(testExistvalue, TokEvaluate) {
+        char *input[2] = {"test"};
+        int index = 0;
+        testToken tes;
+        EXPECT_EQ(tes.isExist(input, index), true);
+}
+
+TEST(tokenizerTest, TokEvaluate) {
+        string input = "Hello World";
+	char *arg[10];
+        Tokenizer(input, arg);
+        testing::internal::CaptureStdout();
+	cout << arg[0] << " " << arg[1];
+	std::string output = testing::internal::GetCapturedStdout();
+        EXPECT_EQ(output, "Hello World");
+}
+
+TEST(AndLogicTest, TokEvaluate) {
+        string input = "Hello && World";
+        char *arg[10];
+	char *temp[10];
+        Tokenizer(input, arg);
+	andToken nd;
+	nd.logic(arg, temp);
+        testing::internal::CaptureStdout();
+        cout << arg[0] << " " << temp[0];
+        std::string output = testing::internal::GetCapturedStdout();
+        EXPECT_EQ(output, "Hello World");
+}
+
+TEST(OrLogicTest, TokEvaluate) {
+        string input = "Hello || World";
+        char *arg[10];
+        char *temp[10];
+        Tokenizer(input, arg);
+        orToken ort;
+        ort.logic(arg, temp);
+        testing::internal::CaptureStdout();
+        cout << arg[0] << " " << temp[0];
+        std::string output = testing::internal::GetCapturedStdout();
+        EXPECT_EQ(output, "Hello World");
+}
+
+TEST(smcolonLogicTest, TokEvaluate) {
+        string input = "Hello ; World";
+        char *arg[10];
+        char *temp[10];
+        Tokenizer(input, arg);
+        smcolonToken smcolon;
+        smcolon.logic(arg, temp);
+        testing::internal::CaptureStdout();
+        cout << arg[0] << " " << temp[0];
+        std::string output = testing::internal::GetCapturedStdout();
+        EXPECT_EQ(output, "Hello World");
+}
+
+TEST(QuoteLogicTest, TokEvaluate) {
+        string input = "\"Hello World\"";
+        char *arg[10];
+        char *temp[10];
+        Tokenizer(input, arg);
+        quoteToken qt;
+        qt.logic(arg, temp);
+        testing::internal::CaptureStdout();
+        cout << arg[0] << " " << arg[1];
+        std::string output = testing::internal::GetCapturedStdout();
+        EXPECT_EQ(output, "Hello World");
+}
+
+TEST(PoundLogicTest, TokEvaluate) {
+        string input = {"Hello # World"};
+        char *arg[10];
+        char *temp[10];
+        Tokenizer(input, arg);
+        poundToken pd;
+        pd.logic(arg, temp);
+        testing::internal::CaptureStdout();
+        cout << arg[0];
+        std::string output = testing::internal::GetCapturedStdout();
+        EXPECT_EQ(output, "Hello");
+}
+
+TEST(ParenLogicTest, TokEvaluate) {
+        string input = {"(Hello) World"};
+        char *arg[10];
+        char *temp[10];
+        Tokenizer(input, arg);
+        parenToken paren;
+        paren.logic(arg, temp);
+        testing::internal::CaptureStdout();
+        cout << arg[0];
+        std::string output = testing::internal::GetCapturedStdout();
+        EXPECT_EQ(output, "Hello");
+}
+
+TEST(BracketLogicTest, TokEvaluate) {
+        string input = {"[ Hello World ]"};
+        char *arg[10];
+        char *temp[10];
+        Tokenizer(input, arg);
+        bracketToken brack;
+        brack.logic(arg, temp);
+        testing::internal::CaptureStdout();
+        cout << arg[0] << " " << arg[1] << " " << arg[2];
+        std::string output = testing::internal::GetCapturedStdout();
+        EXPECT_EQ(output, "test Hello World");
+}
+
+TEST(TestFuncTest, TokEvaluate) {
+        string input = {"test fakedir"};
+        char *arg[10];
+        Tokenizer(input, arg);
+	testToken tes;
+        testing::internal::CaptureStdout();
+        tes.test(arg);
+        std::string output = testing::internal::GetCapturedStdout();
+        EXPECT_EQ(output, "(false)\n");
 }
 
 #endif
